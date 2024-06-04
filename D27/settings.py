099ce33b98ac1a2c62db29b2a2669040f8bc50dd
EnarmPro <enarme365@example.com>
2024-06-03 19:44:59 -0600

    Hosting Funcionando

--- D27/settings.py
+++ D27/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-&2=rpd-toj&x)1nm%yxh*)_)1c!y^g(e_7_!gu_9!=8_hml!$c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
