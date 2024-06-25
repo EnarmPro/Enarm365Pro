7e872b10e0d21f8d92b63f1fed7822d081c69383
EnarmPro <enarme365@gmail.com>
2024-06-24 19:46:09 -0600

    Cambios Finales

--- D27/settings.py
+++ D27/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -101,7 +102,7 @@ DATABASES = {
     }
 }
 """
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -112,8 +113,10 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
-
-
+"""
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
